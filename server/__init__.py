b7545ab4e431b2e01110b22816d6f2d8996fff9a
Parvez M Robin <parvezmrobin@gmail.com>
2022-09-15 14:50:10 -0300

    generate explanations from FineTuned CodeT5

--- server/__init__.py
+++ server/__init__.py
@@ -1,12 +1,15 @@
+import os.path
 import random
-import re
 import uuid
 from dataclasses import dataclass, asdict
 from time import time
-from typing import Dict
+from typing import Dict, Optional
 
+import torch
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
+from torch.nn import functional as F
+from transformers import T5Config, T5ForConditionalGeneration, RobertaTokenizer
 from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
@@ -14,10 +17,22 @@ CORS(app)
 
 
 @dataclass
+class Explanation:
+    explanation: str
+    score: float
+
+
+@dataclass
+class ModelData:
+    name: str
+    file: str
+
+
+@dataclass
 class ModelNames:
-    Bugsplainer: str = '262.finetune-sbt-random-512-64-16-60m'
-    Bugsplainer220M: str = '268.finetune-sbt-random-512-64-16-220m'
-    FineTunedCodeT5: str = '251.finetune-patch-sbt1-large-512-64-16-60m'
+    Bugsplainer: ModelData = ModelData('Bugsplainer', '262.finetune-sbt-random-512-64-16-60m')
+    Bugsplainer220M: ModelData = ModelData('Bugsplainer 220M', '268.finetune-sbt-random-512-64-16-220m')
+    FineTunedCodeT5: ModelData = ModelData('FineTuned Code T5', '251.finetune-patch-sbt1-large-512-64-16-60m')
 
 
 model_names = ModelNames()
@@ -25,8 +40,7 @@ model_names = ModelNames()
 
 @app.route('/models', methods=['GET'])
 def get_model_names():
-    next_ch_cap_re = re.compile('(?<=[a-z])(?=[A-Z0-9])')
-    models = [' '.join(next_ch_cap_re.split(model)) for model in asdict(model_names).keys()]
+    models = [model['name'] for model in asdict(model_names).values()]
     return jsonify(models=models)
 
 
@@ -37,13 +51,71 @@ def explain():
     start = request_data.get('start')
     end = request_data.get('end')
     model = request_data.get('model')
+    num_explanations = request_data.get('num_explanations')
 
-    explanations = [{
-        'score': 1 - (random.random() * 0.1),
-        'explanation': f'This is an explanation from {model} for line {start} to {end}'
-    }]
+    if model == model_names.FineTunedCodeT5.name:
+        explanations = _get_explanations_from_fine_tuned_CodeT5(
+            code, start, end, num_explanations
+        )
+    else:
+        explanations = [Explanation(
+            f'This is an explanation from {model} for line {start} to {end}',
+            1 - (random.random() * 0.1),
+        )]
 
     return jsonify(model=model, explanations=explanations)
+
+
+MODEL_DIR = 'server/models'
+device = torch.device('cuda')
+
+
+def _get_explanations_from_fine_tuned_CodeT5(
+        code: str, start: int, end: int, num_explanations: Optional[int] = None,
+):
+    tokenizer = RobertaTokenizer.from_pretrained('Salesforce/codet5-base')
+    config = T5Config.from_pretrained(os.path.join(MODEL_DIR, 'config_60m.json'))
+    model_path = os.path.join(
+        MODEL_DIR, ModelNames.FineTunedCodeT5.file, 'output', 'checkpoint-best-bleu',
+    )
+    model: T5ForConditionalGeneration = T5ForConditionalGeneration.from_pretrained(
+        model_path, config=config,
+    )
+    model.to(device)
+    model.eval()
+
+    code_lines = [
+        line for i, line in enumerate(code.splitlines(), 1)
+        if start <= i <= end
+    ]
+
+    buggy_code = '\n'.join(code_lines)
+    source_str = f"finetune patch: {buggy_code}"
+    source_ids = tokenizer.encode(
+        source_str, max_length=512, padding='max_length', truncation=True,
+    )
+    source_tensor = torch.tensor([source_ids], dtype=torch.long, device=device)
+    source_mask = source_tensor.ne(tokenizer.pad_token_id)
+
+    outputs = model.generate(
+        inputs=source_tensor,
+        attention_mask=source_mask,
+        early_stopping=True,
+        num_beams=10,
+        num_return_sequences=num_explanations or 3,
+        output_scores=True,
+        return_dict_in_generate=True,
+    )
+
+    explanations = [
+        tokenizer.decode(seq, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+        for seq in outputs.sequences
+    ]
+
+    scores = F.softmax(outputs.sequences_scores, dim=0).tolist()
+    return [
+        Explanation(*param) for param in zip(explanations, scores)
+    ]
 
 
 @app.before_request
