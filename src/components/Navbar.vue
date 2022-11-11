f4a53053e11e03bb9dcdc952c1175545c5871d16
Parvez M Robin <parvezmrobin@gmail.com>
2022-11-11 17:10:00 -0400

    moved Experimental UI toggle

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="navbar bg-light px-5">
     <h3>Bugsplainer</h3>
+    <div
+      class="form-check form-switch bg-danger py-2 pe-2 rounded"
+      style="padding-left: 3rem"
+    >
+      <input
+        id="isExperimental"
+        class="form-check-input"
+        type="checkbox"
+        :value="isExperimental"
+        @input="updateIsExperimental"
+      />
+      <label class="form-check-label text-warning fw-bold" for="isExperimental">
+        Experimental UI
+      </label>
+    </div>
   </div>
   <nav class="navbar sticky-top bg-light">
     <form
@@ -68,21 +83,6 @@
         </button>
       </div>
     </form>
-    <div
-      class="form-check form-switch bg-danger py-2 pe-2 rounded"
-      style="margin-right: 2.5rem; padding-left: 3rem"
-    >
-      <input
-        id="isExperimental"
-        class="form-check-input"
-        type="checkbox"
-        :value="isExperimental"
-        @input="updateIsExperimental"
-      />
-      <label class="form-check-label text-warning fw-bold" for="isExperimental">
-        Experimental UI
-      </label>
-    </div>
   </nav>
 </template>
 
